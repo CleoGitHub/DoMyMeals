c776b936b3ef79f778ee6dda487d124bb8a6bce9
nsoulier <nathan.soulier@hardis-group.com>
2022-03-16 14:15:30 +0100

    prevent login if email is not verified & toasts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { FirebaseApp } from '@angular/fire/app';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,14 @@ export class AuthService {
   // Firebase User
   private connectedUser = new BehaviorSubject(null);
 
-  constructor(private auth: AngularFireAuth, private router: Router) {
+  constructor(private toastController: ToastController, private auth: AngularFireAuth, private router: Router) {
     this.auth.authState.subscribe(user => {
       this.connectedUser.next(user);
     });
   }
 
   isSignedIn(): boolean {
-    return this.connectedUser.value !== null;
+    return this.connectedUser.value !== null && this.connectedUser.value.emailVerified;
  }
 
   getConnectedUser(){
@@ -27,7 +28,13 @@ export class AuthService {
   }
 
   async register(email: string, password:string){
-    await this.auth.createUserWithEmailAndPassword(email, password);
+    await this.auth.createUserWithEmailAndPassword(email, password).catch(error => {
+      this.toastController.create({
+        message: error.message,
+        duration: 3000,
+        color: 'danger'
+      }).then(toast => toast.present());
+    });
     await (await this.auth.currentUser).sendEmailVerification();
     await this.auth.signOut().then(() => {
       this.router.navigate(['/login']);
@@ -36,7 +43,28 @@ export class AuthService {
 
   signIn(email: string, password:string){
     return this.auth.signInWithEmailAndPassword(email, password).then(() => {
-      this.router.navigate(['/']);
+      this.auth.currentUser.then(user => {
+        if (!user.emailVerified) {
+          this.toastController.create({
+            message: 'Veuillez vérifier votre email',
+            duration: 3000,
+            color: 'warning'
+          }).then(toast => toast.present());
+
+          this.auth.signOut().then(() => {
+            this.router.navigate(['/login']);
+          });
+        } else {
+          this.router.navigate(['/']);
+        }
+      });
+    })
+    .catch(error => {
+      this.toastController.create({
+        message: error.message,
+        duration: 3000,
+        color: 'danger'
+        }).then(toast => toast.present());
     });
   }
 
